d7a11effd96254a6307167cbd10f406610ecf9e5
Castro9324603465 <bigboss0613@outlook.jp>
2025-04-01 19:31:39 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    // 必要に応じてビルドの設定を変更
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      "./src/**/*.{astro,html,js}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          yellow: {
+            500: '#F59E0B',
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
+  
